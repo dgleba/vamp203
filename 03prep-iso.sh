37de60f8a9813c499012c4e5e4f37b64236d1abd
David Gleba <dgleba@gmail.com>
2015-10-02 14:01:59 -0400

    2015-10-02_Fri_14.01-PM

--- 03prep-iso.sh
+++ 03prep-iso.sh
@@ -10,12 +10,12 @@ rm create-unattended-iso.sh
 wget https://raw.githubusercontent.com/dgleba/vamp203/master/create-unattended-iso.sh
 
 #test...
-wget https://raw.githubusercontent.com/dgleba/vamp203/master/bootstrap203.sh
-wget https://raw.githubusercontent.com/dgleba/vamp203/master/import203.sh
-wget https://raw.githubusercontent.com/dgleba/vamp203/master/django203.sh
-wget https://raw.githubusercontent.com/dgleba/vamp203/master/start.sh
-wget https://raw.githubusercontent.com/dgleba/vamp203/master/start.sh
-#https://github.com/dgleba/vamp203/blob/master/create-unattended-iso.sh
+#wget https://raw.githubusercontent.com/dgleba/vamp203/master/bootstrap203.sh
+#wget https://raw.githubusercontent.com/dgleba/vamp203/master/import203.sh
+#wget https://raw.githubusercontent.com/dgleba/vamp203/master/django203.sh
+#wget https://raw.githubusercontent.com/dgleba/vamp203/master/start.sh
+#wget https://raw.githubusercontent.com/dgleba/vamp203/master/start.sh
+#blob is not correct.... https://github.com/dgleba/vamp203/blob/master/create-unattended-iso.sh
 
 chmod +x create-unattended-iso.sh
 #  >>>>>>edit it for tmp and iso source
--- 09createvbox.bat
+++ 09createvbox.bat
@@ -5,14 +5,31 @@
 
 set vmname=vamp203b
 set vboxm="C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
+
 %vboxm% createvm --name %vmname% --ostype Ubuntu_64 --register
 
 ..nic problem..
 :%vboxm% modifyvm %vmname% --memory 768 --cpus 1  --acpi on  --nic1 bridged --bridgeadapter1 wlan0 --vrde on --vrdeport 5555 --clipboard bidirectional
-%vboxm% modifyvm %vmname% --memory 768 --cpus 1  --acpi on  --nic1 bridged --bridgeadapter1 eth0  --clipboard bidirectional
-: try %vboxm% modifyvm %vmname%  --nic1 bridged --bridgeadapter1 eth0 
-:%vboxm% modifyvm %vmname% --memory 768 --cpus 1  --acpi on   --vrde on --vrdeport 5555 --clipboard bidirectional
+:Could not start the machine vamp203b because the following physical network interfaces were not found:
+:wlan0 (adapter 1)
 
+:You can either change the machine's network settings or stop the machine.
+: noworky.. %vboxm% modifyvm %vmname%  --nic1 bridged --bridgeadapter1 eth0 
+:%vboxm% modifyvm %vmname%  --vrde on --vrdeport 5555 
+no...
+:%vboxm% modifyvm %vmname%   --nic1 bridged --bridgeadapter3 wlan0
+
+%vboxm% modifyvm %vmname% --memory 768 --cpus 1  --acpi on   --clipboard bidirectional
+
+:nic...
+:http://kappataumu.com/articles/creating-an-Ubuntu-VM-with-packer.html
+%vboxm% list bridgedifs
+: mine on toshiba s55t --   Intel(R) Dual Band Wireless-AC 3160 #2
+:eg:  vboxmanage modifyvm your-vm-name-here --nic1 bridged --bridgeadapter1 "Intel(R) Ethernet Connection I217-V"
+:worked..
+%vboxm% modifyvm %vmname%  --nic1 bridged --bridgeadapter1 "Intel(R) Dual Band Wireless-AC 3160 #2"
+
+:shared folders...
 %vboxm% sharedfolder add %vmname% --name %vmname% --hostpath c:/var/vamp203b/ --automount
 mkdir c:\var\vamp203b
 
@@ -31,19 +48,14 @@ mkdir "C:\Users\%USERNAME%\VirtualBox VMs\%vmname%\"
 %vboxm% modifyvm %vmname% --boot1 dvd
 
 %vboxm% startvm %vmname% 
+
 :# %vboxm% startvm %vmname% --type=headless
 
 :%vboxm% controlvm %vmname% poweroff
 
-:Could not start the machine vamp203b because the following physical network interfaces were not found:
-:wlan0 (adapter 1)
-:You can either change the machine's network settings or stop the machine.
-
-no...
-:%vboxm% modifyvm %vmname%   --nic1 bridged --bridgeadapter3 wlan0
-
+:get info from another vm..
 :%vboxm% import -n C:\0\ubuntu201.ova
 
-:setup the network in gui..........
+:for now... setup the network in gui..........
 
 pause
