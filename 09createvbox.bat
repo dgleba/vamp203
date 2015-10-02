a91b310cb88bfd3208ef3c363a339c6dfff0e1f5
dgleba <dgleba@gmail.com>
2015-10-02 09:13:45 -0400

    2015-10-02 09:13AM

--- 09createvbox.bat
+++ 09createvbox.bat
@@ -1,4 +1,4 @@
-create vbox empty...  [win]
+:create vbox empty...  [win]
 
 :http://zaufi.github.io/administration/2012/08/31/vbox-setup-new-vm/
 :http://www.trimentation.com/wp/?p=100
@@ -15,21 +15,23 @@ set vboxm="C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
 mkdir c:\var\vamp203b
 
 cd \temp
-%vboxm% createhd --filename "C:\Users\dg\VirtualBox VMs\%vmname%\%vmname%.vmdk" --size 10240
+mkdir "C:\Users\%USERNAME%\VirtualBox VMs\%vmname%\"
+
+%vboxm% createhd --filename "C:\Users\%USERNAME%\VirtualBox VMs\%vmname%\%vmname%.vmdk" --size 10240
 
 %vboxm%  storagectl %vmname% --name storage --add sata  --controller IntelAHCI 
 # %vboxm%  storagectl %vmname% --name storage --add sata --controller IntelAHCI --portcount 4 --hostiocache off
 
-%vboxm% storageattach %vmname% --storagectl storage --port 1 --medium "C:\Users\dg\VirtualBox VMs\%vmname%\%vmname%.vmdk" --type hdd
+%vboxm% storageattach %vmname% --storagectl storage --port 1 --medium "C:\Users\%USERNAME%\VirtualBox VMs\%vmname%\%vmname%.vmdk" --type hdd
 
 %vboxm% storageattach %vmname% --storagectl storage --port 2 --medium C:\var\varvamp\files\ubuntu-14.04.3-server-amd64-unattended.iso --type dvddrive
 
 %vboxm% modifyvm %vmname% --boot1 dvd
 
 %vboxm% startvm %vmname% 
-# %vboxm% startvm %vmname% --type=headless
+:# %vboxm% startvm %vmname% --type=headless
 
-%vboxm% controlvm %vmname% poweroff
+:%vboxm% controlvm %vmname% poweroff
 
 :Could not start the machine vamp203b because the following physical network interfaces were not found:
 :wlan0 (adapter 1)
