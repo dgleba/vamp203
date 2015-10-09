65af0c1a7fb3d5f89ddebaacc97c604f74ca4efb
David Gleba <dgleba@gmail.com>
2015-10-08 23:16:23 -0400

    2015-10-08_Thu_23.16-PM

--- tmp1.sh
+++ tmp1.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
+
+
+
+
 get1
 {
-# get zip of repo from github
-cd 
-cd share203
-wget -N https://codeload.github.com/dgleba/vamp203/zip/master
-cd
-#unzip [ -j junk paths - all in one folder ] 
-rm -rf sh
-unzip  ~/share203/master vamp203-master/* -d ./sh
 
+curl --digest --user dgleba:calculat https://bitbucket.org/dgleba/vamp198e/get/master.zip -o vamp198e.zip
 
+192.168.88.86
 
 }
 
@@ -27,6 +25,16 @@ echo (date +"__%Y-%m-%d_%a_%k.%M.%S-%Z")
 offlinestuff()  {
 
 echo tmp1
+
+# get zip of repo from github
+cd 
+cd share203
+wget -N https://codeload.github.com/dgleba/vamp203/zip/master
+cd
+#unzip [ -j junk paths - all in one folder ] 
+rm -rf sh
+unzip  ~/share203/master vamp203-master/* -d ./sh
+
 
 }
 
